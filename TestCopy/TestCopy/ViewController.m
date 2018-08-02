92949059228f7ea87fd9cdfe5b6f609ba0a81b5f
LL <ll@LLdeiMac.local>
2018-08-02 16:18:04 +0800

    1. Update NSDictionary and NSArray test.

--- TestCopy/TestCopy/ViewController.m
+++ TestCopy/TestCopy/ViewController.m
@@ -76,23 +76,41 @@
     
     NSDictionary *testDict1 = [[NSDictionary alloc] initWithObjectsAndKeys:@"value", @"key", nil];
     NSLog(@"testDict1:%p=%@=%@", testDict1, [testDict1 class], testDict1);
+    for (NSString *key in [testDict1 allKeys]) {
+        NSLog(@"value1:%p=%@",testDict1[key], testDict1[key]);
+    }
     
     id testDict2 = [testDict1 copy];
     NSLog(@"testDict2:%p=%@=%@", testDict2, [testDict2 class], testDict2);
+    for (NSString *key in [testDict2 allKeys]) {
+        NSLog(@"value2:%p=%@",testDict2[key], testDict2[key]);
+    }
     
     id testDict3 = [testDict1 mutableCopy];
     NSLog(@"testDict3:%p=%@=%@", testDict3, [testDict3 class], testDict3);
+    for (NSString *key in [testDict3 allKeys]) {
+        NSLog(@"value3:%p=%@",testDict3[key], testDict3[key]);
+    }
     
     printf("\n");
     
     NSMutableDictionary *testMuDict1 = [[NSMutableDictionary alloc] initWithObjectsAndKeys:@"value", @"key", nil];
     NSLog(@"testMuDict1:%p=%@=%@", testMuDict1, [testMuDict1 class], testMuDict1);
+    for (NSString *key in [testMuDict1 allKeys]) {
+        NSLog(@"value1:%p=%@",testMuDict1[key], testMuDict1[key]);
+    }
     
     id testMuDict2 = [testDict1 copy];
     NSLog(@"testMuDict2:%p=%@=%@", testMuDict2, [testMuDict2 class], testMuDict2);
+    for (NSString *key in [testMuDict2 allKeys]) {
+        NSLog(@"value2:%p=%@",testMuDict2[key], testMuDict2[key]);
+    }
     
     id testMuDict3 = [testDict1 mutableCopy];
     NSLog(@"testMuDict3:%p=%@=%@", testMuDict3, [testMuDict3 class], testMuDict3);
+    for (NSString *key in [testMuDict3 allKeys]) {
+        NSLog(@"value3:%p=%@",testMuDict3[key], testMuDict3[key]);
+    }
 }
 
 /**
@@ -103,23 +121,41 @@
     
     NSArray *testArray1 = [NSArray arrayWithObjects:@"testArray1", nil];
     NSLog(@"testArray1:%p=%@=%@", testArray1, [testArray1 class], testArray1);
+    for (id object in testArray1) {
+        NSLog(@"value1:%p=%@",object, object);
+    }
     
     id testArray2 = [testArray1 copy];
     NSLog(@"testArray2:%p=%@=%@", testArray2, [testArray2 class], testArray2);
+    for (id object in testArray2) {
+        NSLog(@"value2:%p=%@",object, object);
+    }
     
     id testArray3 = [testArray1 mutableCopy];
     NSLog(@"testArray3:%p=%@=%@", testArray3, [testArray3 class], testArray3);
+    for (id object in testArray3) {
+        NSLog(@"value3:%p=%@",object, object);
+    }
     
     printf("\n");
     
     NSMutableArray *testMuArray1 = [[NSMutableArray alloc] initWithObjects:@"testMuArray1", nil];
     NSLog(@"testMuArray1:%p=%@=%@", testMuArray1, [testMuArray1 class], testMuArray1);
+    for (id object in testMuArray1) {
+        NSLog(@"value1:%p=%@",object, object);
+    }
     
     id testMuArray2 = [testMuArray1 copy];
     NSLog(@"testMuArray2:%p=%@=%@", testMuArray2, [testMuArray2 class], testMuArray2);
+    for (id object in testMuArray2) {
+        NSLog(@"value2:%p=%@",object, object);
+    }
     
     id testMuArray3 = [testMuArray1 mutableCopy];
     NSLog(@"testMuArray3:%p=%@=%@", testMuArray3, [testMuArray3 class], testMuArray3);
+    for (id object in testMuArray3) {
+        NSLog(@"value3:%p=%@",object, object);
+    }
 }
 
 /**
