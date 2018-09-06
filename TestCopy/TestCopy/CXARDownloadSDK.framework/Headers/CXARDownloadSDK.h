b44fd9ec5cece560a96810ee07dbd6131db21780
LL <ll@LLdeiMac.local>
2018-09-06 10:55:04 +0800

    1、增加Framework

---
+++ TestCopy/TestCopy/CXARDownloadSDK.framework/Headers/CXARDownloadSDK.h
@@ -0,0 +1,9 @@
+//
+//  CXARDownloadSDK.h
+//  CXARDownloadSDK
+//
+//  Created by LL on 2018/2/26.
+//  Copyright © 2018年 LL. All rights reserved.
+//
+
+#import <CXARDownloadSDK/CXARDownloadManager.h>
---
+++ TestCopy/TestCopy/CXARSDK.framework/Headers/CXARSDK.h
@@ -0,0 +1,14 @@
+//
+//  CXARSDK.h
+//  CXARSDK
+//
+//  Created by LL on 2018/1/30.
+//  Copyright © 2018年 LL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+#import <CXARSDK/CXARManager.h>
+#import <CXARSDK/CXARCameraVC.h>
