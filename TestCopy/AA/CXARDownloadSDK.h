e8902f2c1a88200f66ea5c58123f6a76f037d8e9
LL <ll@LLdeiMac.local>
2018-09-06 14:18:54 +0800

    1、修改库引用方式

--- TestCopy/AA/CXARDownloadSDK.h
+++
@@ -1,9 +0,0 @@
-//
-//  CXARDownloadSDK.h
-//  CXARDownloadSDK
-//
-//  Created by LL on 2018/2/26.
-//  Copyright © 2018年 LL. All rights reserved.
-//
-
-#import <CXARDownloadSDK/CXARDownloadManager.h>
--- TestCopy/AA/CXARZipSDK.h
+++
@@ -1,9 +0,0 @@
-//
-//  CXARZipSDK.h
-//  CXARZipSDK
-//
-//  Created by LL on 2018/1/30.
-//  Copyright © 2018年 LL. All rights reserved.
-//
-
-#import <CXARZipSDK/CXARZipArchive.h>
